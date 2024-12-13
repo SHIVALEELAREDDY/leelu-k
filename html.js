41013919d895dfe81f305dc6e0fd3db2137a4570
SHIVALEELAREDDY <itsshivureddy956@gmail.com>
2024-12-13 20:52:57 +0530

    files

---
+++ html.js
@@ -0,0 +1,30 @@
+function changetext(){
+    let h1tag=document.getElementById("h1")
+    console.log(h1tag)
+    h1tag.style.textcontent="hello everone";
+    h1tag.style.backgroundColor="red";
+    h1tag.style.padding="60px";
+    h1tag.style.cursor="pointer"
+    h1tag.style.boxShadow="2px 2px 5px black";
+    h1tag.style.borderRadius="100px"
+}
+function changetextondblclick(){
+    let h2tag=document.getElementById("h2")
+    console.log(h2tag)
+    h2tag.style.textcontent="hello everone";
+    h2tag.style.backgroundColor="red";
+    h2tag.style.padding="60px";
+    h2tag.style.cursor="pointer"
+    h2tag.style.boxShadow="2px 2px 5px black";
+    h2tag.style.borderRadius="100px"
+}
+function changetextonmouseover(){
+    let h3tag=document.getElementById("h3")
+    console.log(h3tag)
+    h3tag.style.textcontent="hello everone";
+    h3tag.style.backgroundColor="red";
+    h3tag.style.padding="60px";
+    h3tag.style.cursor="pointer"
+    h3tag.style.boxShadow="2px 2px 5px black";
+    h3tag.style.borderRadius="100px"
+}
